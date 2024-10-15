8c3f8a110743665b637029da4105fbe7c0adefe6
Avery F <avery@averyf.dev>
2024-10-15 19:32:46 -0400

    more updates to content

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -9,9 +9,15 @@ export const content = {
   },
   bodies: {
     about:
-      "One of those weirdos that actually likes their job, Avery is most interested in full stack web development. \n\nProficient in many languages and frameworks including TypeScript, JavaScript, Python, and Java with React, Angular, and SpringBoot respectively. \n\nThey are also an avid climber 🧗, runner 🏃, and hiker 🥾 when they can spare the time.",
+      `One of those weirdos that actually likes their job.\
+      
+      Proficient in many languages, frameworks, and libraries including  the usual suspects: Java with SpringBoot, TypeScript React, and numerous flavors of SQL. \
+      
+      An avid climber 🧗, runner 🏃, and hiker 🥾 when I can spare the time.`,
     contact:
-      "If you want to reach me, my personal email is: \navery [at] averyf [dot] dev",
+      `If you want to reach me, my personal email is: 
+      
+      avery [at] averyf [dot] dev`,
   },
 };
 
@@ -28,16 +34,16 @@ export const work: Job[] = [
     company: "Attentive Mobile",
     title: "Software Engineer I - Tactical Engineering",
     description: [
-      `Designed and developed a new SMS-distributed digital wallet loyalty product - creating a new revenue stream 
+      `Designed and developed a new SMS-distributed digital wallet loyalty product - creating a new revenue stream \
         per client of $3k+ a month, in addition to the engineering hours charged, which continues to attract new brands.`,
-      `Re-designed and re-wrote large swaths of existing SMS-distributed digital coupons functionality that eliminated 
+      `Re-designed and re-wrote large swaths of existing SMS-distributed digital coupons functionality that eliminated \
         70% of billed third party vendor calls, allowing us to pass on that savings to one of our biggest clients.`,
-      `Proposed new team-wide interface-driven design patterns that have since been adopted across the majority of 
+      `Proposed new team-wide interface-driven design patterns that have since been adopted across the majority of \
         Tactical-owned services, standardizing large portions of the code and simplifying collaboration.`,
-      `Adapted company standard cursor-based pagination pattern to support sorting based on multiple fields, including 
-        arbitrary fields like strings, delivering functionality requested by clients and enabling native compatibility with 
+      `Adapted company standard cursor-based pagination pattern to support sorting based on multiple fields, including \
+        arbitrary fields like strings, delivering functionality requested by clients and enabling native compatibility with \
         graph-connected UI components provided by our platform team.`,
-      `Coordinated between our team and several other engineering teams to add additional functionality to their 
+      `Coordinated between our team and several other engineering teams to add additional functionality to their \
         domain-specific services to unblock a project automating the onboarding of over 10,000 new billable company accounts.`,
     ],
     logo: attnLogo,
@@ -47,9 +53,9 @@ export const work: Job[] = [
     company: "Attentive Mobile",
     title: "Solutions Engineer II - Solutions Engineering",
     description: [
-      `Designed and developed self-service fullstack solutions to cover gaps in the product or as internal tools, 
+      `Designed and developed self-service fullstack solutions to cover gaps in the product or as internal tools, \
       ultimately eliminating around 15 ticket types and creating team bandwidth.`,
-      `Worked effectively across engineering teams to contribute code to their domain-specific services, in line 
+      `Worked effectively across engineering teams to contribute code to their domain-specific services, in line \
       with their best-practices and existing infrastructure.`,
     ],
     logo: attnLogo,
@@ -59,9 +65,9 @@ export const work: Job[] = [
     company: "Attentive Mobile",
     title: "Solutions Engineer I - Solutions Engineering",
     description: [
-      `Completed numerous daily support tasks manually that were not covered by the product or any internal tools, 
+      `Completed numerous daily support tasks manually that were not covered by the product or any internal tools, \
         leading to continued client satisfaction and preventing churn.`,
-      `Collaborated with client-facing staff to translate client requests into actionable work and translate technical 
+      `Collaborated with client-facing staff to translate client requests into actionable work and translate technical \
         concepts for all involved.`,
     ],
     logo: attnLogo,
