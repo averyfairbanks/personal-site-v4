2919ea9785a22f2ec421a5d1d1cbfbf5da721847
Avery F <avery@averyf.dev>
2025-10-06 11:09:48 -0400

    welcome anim working

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,33 +1,36 @@
-import { Link } from 'react-router-dom';
-import { content } from '../resources/content';
-import '../styles/App.css';
+import { Link } from 'react-router-dom'
+import { content } from '../resources/content'
+import '../styles/App.css'
 
-import { animate, splitText, stagger } from 'animejs';
-import { useEffect } from 'react';
+import { animate, splitText, stagger } from 'animejs'
+import { useEffect } from 'react'
 
 const Home: React.FC = () => {
-  const {
-    welcome: { title },
-  } = content;
+    const {
+        welcome: { title },
+    } = content
 
-  useEffect(() => {});
+    useEffect(() => {
+        const { chars } = splitText('.anim-target', {
+            words: false,
+            chars: true,
+        })
 
-  const { chars } = splitText('.anim-target', { words: false, chars: true });
+        animate(chars, {
+            x: [{ to: 10, delay: stagger(30), ease: 'inOutElastic' }],
+            duration: 1200,
+            // delay: stagger(30),
+            loop: true,
+            alternate: true,
+            autoplay: true,
+        })
+    }, [])
 
-  animate(chars, {
-    x: [{ to: 10, ease: 'inOutElastic' }],
-    duration: 1600,
-    delay: stagger(30),
-    loop: true,
-    alternate: true,
-    autoplay: true,
-  });
+    return (
+        <Link className="title clean-link anim-target" to="/about">
+            {title}
+        </Link>
+    )
+}
 
-  return (
-    <Link className="title clean-link anim-target" to="/about">
-      {title}
-    </Link>
-  );
-};
-
-export default Home;
+export default Home
