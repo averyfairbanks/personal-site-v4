521410616be7f90251be8a187dc522468677778d
Avery F <avery@averyf.dev>
2025-10-07 10:06:06 -0400

    init

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,27 +4,36 @@ import "../styles/App.css";
 
 import { animate, splitText, stagger } from "animejs";
 import { useEffect } from "react";
+import useWindowDimensions from "../util/useWindowDimensions";
 
 const Home: React.FC = () => {
+  const { innerWidth } = useWindowDimensions();
+  
   const {
     welcome: { title },
   } = content;
+  
+  const { chars } = splitText(".anim-target", {
+    words: false,
+    chars: true,
+  });
+  
+  const anim = animate(chars, {
+    x: [{ to: 10, delay: stagger(30), ease: "inOutElastic" }],
+    duration: 1200,
+    loop: true,
+    alternate: true,
+    autoplay: true,
+  });
 
   useEffect(() => {
-    const { chars } = splitText(".anim-target", {
-      words: false,
-      chars: true,
-    });
-
-    animate(chars, {
-      x: [{ to: 10, delay: stagger(30), ease: "inOutElastic" }],
-      duration: 1200,
-      // delay: stagger(30),
-      loop: true,
-      alternate: true,
-      autoplay: true,
-    });
-  }, []);
+    if (innerWidth <= 630) {
+      anim.cancel();
+      anim.revert();
+    } else {
+      anim.resume();
+    }
+  }, [innerWidth]);
 
   return (
     <Link className="title clean-link anim-target" to="/about">
